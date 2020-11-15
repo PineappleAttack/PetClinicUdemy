220f54c32732c3586cbc4a7a4b5a8b52811601d7
Your Name <you@example.com>
2020-11-15 21:03:42 +0100

    init pet clinic udemy

---
+++ src/main/java/com/example/PetClinicUdemy/PetClinicUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetClinicUdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicUdemyApplication.class, args);
+	}
+
+}
